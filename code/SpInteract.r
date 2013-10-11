d85e943455503f806b91f4db7869cc3e4c085ff8
Adam M. Wilson <adammichaelwilson@gmail.com>
2013-10-11 17:01:10 -0400

    Fixing posterior analysis

--- code/SpInteract.r
+++ code/SpInteract.r
@@ -1,5 +1,5 @@
 library(spBayes);library(MASS);library(MBA);library(fields)
-
+library(reshape);library(latticeExtra)
 
 ################################
 ##Spatial multivariate poisson
@@ -9,7 +9,7 @@ n <- 100 # number of locations
 q <- 3   # number of 'species'
 
 #make some locations
-coords <- cbind(runif(n,1,100),runif(n,1,100))  
+coords <- cbind.data.frame(x=runif(n,1,100),y=runif(n,1,100))  
 
 ## generate the knots at which the spatial effects are captured
 if(n<=100) knots=coords
@@ -34,7 +34,7 @@ Psi <- diag(0,q)
 ## Construct multivariate predictive process covariance matrices
 ## following example in spBayes spMvGLM function
 ## in this case the knots are the same as the points, so essentially this is just a 'normal' point-process model...
-c1 <- mkSpCov(coords=coords, K=K, Psi=Psi,theta=theta,cov.model="exponential")
+c1 <- mkSpCov(coords=as.matrix(coords), K=K, Psi=Psi,theta=theta,cov.model="exponential")
 
 ## generate the spatial-species effects for each species in each location
 ## note this is a n x q vector 
@@ -49,28 +49,28 @@ beta <- c(-1,0,1)
 
 ## generate the response data - counts of each species at each point
 y <- rpois(n*q, exp(X%*%beta+w))
-## convert that to a location x species matrix of abundances
-y=matrix(y,ncol=q,byrow=T)
+spind=rep(1:q,each=n)
 
-## y is now our species abundance data for three species
-head(y)
+## convert that to ("wide") location x species matrix of abundances
+yw=matrix(y,ncol=q,byrow=T)
+## yw is now our species abundance data for three species
+head(yw)
 
-## build spatial dataframe for plotting
-#d=data.frame(x=coords[,1],y=coords[,2],spid=spid,abun=y)
-#coordinates(d)=c("x","y")
+### convert to presence absence
+yw2=ifelse(yw>0,1,0)
+head(yw2)
 
 ## look at species locations and abundances
-scale=max(y)/8  #simple scale to make circles pleasing, adjust if too big or too small
-plot(coords,cex=y[,1]/scale,col="red",main="Example species abundances at various locations generated with species interaction matrix",ylab="Y",xlab="X")
-points(coords,cex=y.2/scale,col="green")
-points(coords,cex=y.3/scale,col="blue")
-points(knots,pch=3)
+scale=max(yw)/8  #simple scale to make circles pleasing, adjust if too big or too small
+plot(coords,cex=yw[,1]/scale,col="red",main="Example species abundances at various locations generated with species interaction matrix",ylab="Y",xlab="X")
+points(coords,cex=yw[,2]/scale,col="green")
+points(coords,cex=yw[,3]/scale,col="blue")
 legend("bottomright",legend=c("Species 1","Species 2","Species 3"),text.col=c("red","green","blue"))
 ## look at the interaction matrix
 cor(K)
 
 ## see how species abundance is correlated 
-splom(cbind(y.1,y.2,y.3))
+splom(yw)
 
 ############################################################################
 ############################################################################
@@ -82,59 +82,76 @@ splom(cbind(y.1,y.2,y.3))
 A.starting <- diag(1,q)[lower.tri(diag(1,q), TRUE)]
 beta.starting <- coefficients(glm(y~X-1, family="poisson"))
 beta.tuning <- t(chol(vcov(glm(y~X-1, family="poisson"))))
-n.samples <- 100000
+n.samples <- 10000
 
-m.1 <- spMvGLM(list(y.1~1,y.2~1,y.3~1), family="poisson", coords=coords,
+## Make formulas  - need to update this to match environmental data 
+frmls=lapply(1:q,function(i) as.formula(paste("yw[,",i,"]~1",sep="")))
+
+m.1 <- spMvGLM(frmls, family="poisson", coords=as.matrix(coords),
                starting= list("beta"=beta.starting, "phi"=rep(0.06,q),
                  "A"=A.starting, "w"=0),
                tuning=list("beta"=beta.tuning, "phi"=rep(0.01,q),
                  "A"=rep(0.005,nltr), "w"=0.001),
-               priors=list("beta.Flat", "phi.Unif"=rep(c(0.03, 0.3),q),
+               priors=list("beta.Flat", "phi.Unif"=list(rep(0.03,q),rep(0.3,q)),
                  "K.IW"=list(q+1, diag(0.1,q))),
                cov.model="exponential",
-               n.samples=n.samples, sub.sample=c(n.samples/2,n.samples,10),
+               n.samples=n.samples,
                verbose=TRUE, n.report=500)
 
 ######################################
 ### Summarize posteriors
 
-m.1$p.samples[,paste("phi_",1:q,sep="")] <- 3/m.1$p.samples[,paste("phi_",1:q,sep="")]
+## check out the chains
+xyplot(m.1$p.beta.theta.samples)
 
-print(summary(mcmc(m.1$p.samples)))
-beta.hat <- colMeans(m.1$p.samples[,1:q])
-w.hat <- rowMeans(m.1$sp.effects)
-y.hat <- exp(X%*%beta.hat+w.hat)
-y.hat.1 <- y.hat[seq(1,length(y.hat),q)]
-y.hat.2 <- y.hat[seq(2,length(y.hat),q)]
-y.hat.3 <- y.hat[seq(3,length(y.hat),q)]
+## create new object with just the posterior samples
+post=window(m.1$p.beta.theta.samples,start=n.samples*.75)
+
+
+#m.1$p.samples[,paste("phi_",1:q,sep="")] <- 3/m.1$p.samples[,paste("phi_",1:q,sep="")]
+#colnames(m.1$p.beta.theta.samples)
+print(summary(mcmc(post)))
+beta.hat <- apply(post[,1:q],2,mean)
+w.hat <- rowMeans(m.1$p.w.samples)
+y.hat <- exp((X%*%beta.hat)+w.hat)
+## reshape it for easier plotting
+#y.hat=matrix(y.hat,byrow=T,nrow=n)
+#w.hat=matrix(w.hat,byrow=T,nrow=n)
 
 ## get posterior species correlation matrix
 Khat <- matrix(0,q,q)
-Khat[lower.tri(Khat,TRUE)] <- colMeans(m.1$p.samples[,grep("K",colnames(m.1$p.samples))])
+Khat[lower.tri(Khat,TRUE)] <- colMeans(post[,grep("K",colnames(post))])
 Khat <- Khat%*%t(Khat)
 cor(Khat) # posterior matrix
 cor(K)  # original matrix
 
 ### CI on interaction matrix
-for(q in c(.025,.5,.975)) {
-  Khat <- matrix(0,q,q)
-  Khat[lower.tri(Khat,TRUE)]
-  apply(m.1$p.samples[,grep("K",colnames(m.1$p.samples))],2,function(i) quantile(i,q))
+#for(qn in c(.025,.5,.975)) {
+#  Khat <- matrix(0,qn,qn)
+#  Khat[lower.tri(Khat,TRUE)]
+#  apply(post[,grep("K",colnames(post))],2,function(i) quantile(i,qn))
+#}
 
+### Build a single dataframe to hold the observed and predicted
+dp=cbind.data.frame(coords,sp=spind,y=y,yhat=y.hat,w=w.hat)
 
-summary(lm((as.vector(cor(K))~as.vector(cor(Khat)))))
+dpl=melt(dp,id.vars=c("x","y","sp"))
+
+xyplot(yhat~y,data=dp,groups=sp)+layer(panel.abline(0,1))
 
 ##Take a look at spatial patterns
-par(mfrow=c(3,2))
-surf <- mba.surf(cbind(coords,y.1),no.X=100, no.Y=100, extend=TRUE)$xyz.est
+par(mfrow=c(1,2))
+surf <- mba.surf(cbind(coords,yw[,1]),no.X=100, no.Y=100, extend=TRUE)$xyz.est
 image(surf, main="Observed counts")
 contour(surf, drawlabels=FALSE, add=TRUE)
-text(coords, labels=y.1, cex=1, col="blue")
-surf <- mba.surf(cbind(coords,y.hat.1),no.X=100, no.Y=100, extend=TRUE)$xyz.est
+text(coords, labels=yw[,1], cex=1, col="blue")
+
+
+surf <- mba.surf(cbind(coords,yw.hat[,1]),no.X=100, no.Y=100, extend=TRUE)$xyz.est
 image(surf, main="Fitted counts")
 contour(surf, add=TRUE)
 contour(surf, drawlabels=FALSE, add=TRUE)
-text(coords, labels=round(y.hat.1,0), cex=1, col="blue")
+text(coords, labels=round(yw.hat[,1],0), cex=1, col="blue")
 surf <- mba.surf(cbind(coords,y.2),no.X=100, no.Y=100, extend=TRUE)$xyz.est
 image(surf)
 contour(surf, drawlabels=FALSE, add=TRUE)
